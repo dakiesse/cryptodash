d6bacce774eaf26bf6c92cfa8a160e2201e108d6
Danil Chekalin <dakiesse@gmail.com>
2018-12-10 23:00:42 +0200

    restructure project

---
+++ src/App/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class Index extends Component {
+  render () {
+    return (
+      <div> Hello world </div>
+    )
+  }
+}
+
+export default Index
