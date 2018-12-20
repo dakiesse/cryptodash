ea7d3a261575238065a95bf7b144d31b3ba2e3a3
Danil Chekalin <dakiesse@gmail.com>
2018-12-20 12:59:11 +0200

    add Tile for CoinGrid; change styles in components

--- src/Settings/ConfirmButton.js
+++ src/Settings/ConfirmButton.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import { AppContext } from '../App/AppProvider'
 import styled from 'styled-components'
-
-const CenterBlock = styled.div`
-  margin: 20px;
-  color: green;
-`
+import { fontSize1, greenBoxShadow, color3 } from '../Shared/Styles'
 
 const ConfirmButtonStyled = styled.div`
+  margin: 20px;
+  color: ${color3};
+  ${fontSize1};
+  padding: 5px;
+  cursor: pointer;
+  
+  &:hover {
+    ${greenBoxShadow};
+  }
+`
+
+const CenterBlock = styled.div`
   display: grid;
   justify-content: center;
-  cursor: pointer;
 `
 
 export default () => (
