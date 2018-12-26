c4cc3cfcbde65208be96ab75b780e74ae7fe72cd
Danil Chekalin <dakiesse@gmail.com>
2018-12-26 16:17:18 +0200

    simple DRY

--- src/Shared/CoinImage.js
+++ src/Shared/CoinImage.js
@@ -4,18 +4,21 @@ import { LazyImage } from 'react-lazy-images'
 const loaderUrl = 'https://samherbert.net/svg-loaders/svg-loaders/oval.svg'
 
 export default function CoinImage ({ coin, style, wasRendered = false }) {
+  const imageLink = `http://cryptocompare.com/${coin.ImageUrl}`
+  style = style || { height: '50px' }
+
   if (wasRendered) {
     return (
-      <img src={`http://cryptocompare.com/${coin.ImageUrl}`} alt={coin.CoinSymbol} style={style || { height: '50px' }}/>
+      <img src={imageLink} alt={coin.CoinSymbol} style={style}/>
     )
   }
 
   return (
     <LazyImage
-      src={`http://cryptocompare.com/${coin.ImageUrl}`}
+      src={imageLink}
       alt={coin.CoinSymbol}
       placeholder={({ imageProps, ref }) => (<img ref={ref} src={loaderUrl} alt={imageProps.alt}/>)}
       actual={({ imageProps }) => (<img alt={imageProps.alt} {...imageProps}/>)}
-      style={style || { height: '50px' }}/>
+      style={style}/>
   )
 }
