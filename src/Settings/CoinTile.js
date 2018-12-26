4e39abdd552f0c1574e8a1a9bf4d538e4576b434
Danil Chekalin <dakiesse@gmail.com>
2018-12-26 16:05:09 +0200

    the render (second+) of CoinImage was optimized

--- src/Settings/CoinTile.js
+++ src/Settings/CoinTile.js
@@ -4,11 +4,16 @@ import { DeletableTile, DisabledTile, SelectableTile } from '../Shared/Tile'
 import CoinHeaderGrid from './CoinHeaderGrid'
 import CoinImage from '../Shared/CoinImage'
 
+// The CoinImage component will be re-rendered with LazyImage
+// behave all the time. To avoid this the code will fix the very
+// first render. And fixing will occur at the module level!
+const wasRenderedForSymbol = new Set()
+
 function clickCoinHandler (topSection, coinKey, addCoin, removeCoin) {
   return () => topSection ? removeCoin(coinKey) : addCoin(coinKey)
 }
 
-export default ({ coinKey, topSection }) => {
+export default function CoinTile ({ coinKey, topSection }) {
   return (
     <AppContext.Consumer>
       {({ coinList, addCoin, removeCoin, hasFavorites }) => {
@@ -21,10 +26,16 @@ export default ({ coinKey, topSection }) => {
           TileClass = DisabledTile
         }
 
+        let wasRendered = true
+        if (!wasRenderedForSymbol.has(coin.Symbol)) {
+          wasRenderedForSymbol.add(coin.Symbol)
+          wasRendered = false
+        }
+
         return (
           <TileClass onClick={clickCoinHandler(topSection, coinKey, addCoin, removeCoin)}>
             <CoinHeaderGrid topSection={topSection} name={coin.CoinName} symbol={coin.Symbol}/>
-            <CoinImage coin={coin}/>
+            <CoinImage coin={coin} wasRendered={wasRendered}/>
           </TileClass>
         )
       }}
--- src/Shared/CoinImage.js
+++ src/Shared/CoinImage.js
@@ -3,11 +3,20 @@ import { LazyImage } from 'react-lazy-images'
 
 const loaderUrl = 'https://samherbert.net/svg-loaders/svg-loaders/oval.svg'
 
-export default ({ coin, style }) => (
-  <LazyImage
-    src={`http://cryptocompare.com/${coin.ImageUrl}`}
-    alt={coin.CoinSymbol}
-    placeholder={({ imageProps, ref }) => (<img ref={ref} src={loaderUrl} alt={imageProps.alt}/>)}
-    actual={({ imageProps }) => (<img alt={imageProps.alt} {...imageProps}/>)}
-    style={style || { height: '50px' }}/>
-)
+export default function CoinImage ({ coin, style, wasRendered = false }) {
+  if (wasRendered) {
+    debugger
+    return (
+      <img src={`http://cryptocompare.com/${coin.ImageUrl}`} alt={coin.CoinSymbol} style={style || { height: '50px' }}/>
+    )
+  }
+
+  return (
+    <LazyImage
+      src={`http://cryptocompare.com/${coin.ImageUrl}`}
+      alt={coin.CoinSymbol}
+      placeholder={({ imageProps, ref }) => (<img ref={ref} src={loaderUrl} alt={imageProps.alt}/>)}
+      actual={({ imageProps }) => (<img alt={imageProps.alt} {...imageProps}/>)}
+      style={style || { height: '50px' }}/>
+  )
+}
