34216ccee17601928461b346b5d6961e6d5da3cf
Danil Chekalin <dakiesse@gmail.com>
2018-12-26 16:12:49 +0200

    cleanup

--- src/Shared/CoinImage.js
+++ src/Shared/CoinImage.js
@@ -5,7 +5,6 @@ const loaderUrl = 'https://samherbert.net/svg-loaders/svg-loaders/oval.svg'
 
 export default function CoinImage ({ coin, style, wasRendered = false }) {
   if (wasRendered) {
-    debugger
     return (
       <img src={`http://cryptocompare.com/${coin.ImageUrl}`} alt={coin.CoinSymbol} style={style || { height: '50px' }}/>
     )
