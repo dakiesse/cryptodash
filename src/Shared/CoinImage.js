4bca92a03302fb4cab5a7be42786ae64b27ef418
Danil Chekalin <dakiesse@gmail.com>
2018-12-26 13:47:10 +0200

    add lazy images

--- src/Shared/CoinImage.js
+++ src/Shared/CoinImage.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import { LazyImage } from 'react-lazy-images'
+
+const loaderUrl = 'https://samherbert.net/svg-loaders/svg-loaders/oval.svg'
 
 export default ({ coin, style }) => (
-  <img src={`http://cryptocompare.com/${coin.ImageUrl}`} alt={coin.CoinSymbol} style={style || { height: '50px' }}/>
+  <LazyImage
+    src={`http://cryptocompare.com/${coin.ImageUrl}`}
+    alt={coin.CoinSymbol}
+    placeholder={({ imageProps, ref }) => (<img ref={ref} src={loaderUrl} alt={imageProps.alt}/>)}
+    actual={({ imageProps }) => (<img alt={imageProps.alt} {...imageProps}/>)}
+    style={style || { height: '50px' }}/>
 )
